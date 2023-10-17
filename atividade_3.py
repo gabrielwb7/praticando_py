8ace17ad6956c4b7d1970e7d7771662e3c746475
Gabriel Rocha <fernandeswb7@hotmail.com>
2023-10-17 17:15:49 -0300

    atividade 3

---
+++ atividade_3.py
@@ -0,0 +1,11 @@
+
+valor1 = input('Digite o primeiro valor: ')
+valor2 = input('Digite o segundo valor: ')
+
+if valor1 > valor2:
+    print(f'{valor1} é maior que o valor {valor2}')
+elif valor1 < valor2:
+    print(f'{valor1} é menor que o valor {valor2}')
+else:
+    print(f'{valor1} é igual o valor {valor2}')
+
