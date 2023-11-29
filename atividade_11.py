9934d54fa203cd4d092d12db421ea6932c3e3e1d
Gabriel Rocha <71228427+gabrielwb7@users.noreply.github.com>
2023-11-29 18:38:27 -0300

    ajuste para pegar os numeros

--- atividade_11.py
+++ atividade_11.py
@@ -1,22 +1,20 @@
-cpf = input("Digite o seu cpf: ")
-cpf_somente_numeros = []
-
-for caractere in cpf: 
-    if caractere.isdigit(): 
-        cpf_somente_numeros.append(int(caractere))
+import re
 
 def calculo_digitos_finais_cpf(numeros_cpf, qtd_indices): 
     contador = qtd_indices
     soma_total = 0
     index = 0
     while contador > 1:
-        soma_total += numeros_cpf[index] * contador
+        soma_total += int(numeros_cpf[index]) * contador
         contador-=1
         index+=1
     
-    primeiro_digito = (soma_total * 10) % 11
+    digito = (soma_total * 10) % 11
 
-    return primeiro_digito if primeiro_digito <= 9 else 0
+    return digito if digito <= 9 else 0
+
+cpf_entrada = input("Digite o seu cpf: ")
+cpf_somente_numeros = re.sub(r'[^0-9]', '', cpf_entrada)
 
 primeiro_digito = calculo_digitos_finais_cpf(cpf_somente_numeros,10)
 segundo_digito = calculo_digitos_finais_cpf(cpf_somente_numeros,11)
